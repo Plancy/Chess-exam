f061fa3d59d6b216bfc2db582fec894e0f0c3cb2
Jericho <dontjericho@gmail.com>
2026-02-20 07:51:57 -0800

    Fix

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/chess/',
+  base: '/Chess-exam/',
   plugins: [react()],
 });
